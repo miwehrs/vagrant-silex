ebd70e1ce56cbfed014dd89be688640336dfc53b
unknown <wehrstei@WEHRSTEI1.emea.hpqcorp.net>
2015-02-04 11:12:53 +0100

    Bootstrap-Seite: lokales Bootstrap-Stylesheet

--- silex/web/templates/layout.html.php
+++ silex/web/templates/layout.html.php
@@ -12,49 +12,49 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-inverse">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="#">
-                    Bootstrap-Site
-                </a>
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-            </div>
-            <div class="collapse navbar-collapse" id="navbar">
-                <ul class="nav navbar-nav">
-                    <li <?= $title == "Home" ? 'class="active"' : "" ?>>
-                        <a href="/home">
-                            <span class="glyphicon glyphicon-home" aria-hidden="true"></span>
-                            Home
-                            <span class="sr-only">(current)</span></a></li>
-                    <li <?= $title == "Music" ? 'class="active"' : "" ?>>
-                        <a href="/music">
-                            <span class="glyphicon glyphicon-music" aria-hidden="true"></span>
-                            Music
-                        </a></li>
-                    <li <?= $title == "Books" ? 'class="active"' : "" ?>>
-                        <a href="/books">
-                            <span class="glyphicon glyphicon-book" aria-hidden="true"></span>
-                            Books
-                        </a></li>
-                    <li <?= $title == "Settings" ? 'class="active"' : "" ?>>
-                        <a href="/settings">
-                            <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
-                            Settings
-                        </a></li>
-                </ul>
-            </div>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">
+                Bootstrap-Site
+            </a>
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
         </div>
-    </nav>
+        <div class="collapse navbar-collapse" id="navbar">
+            <ul class="nav navbar-nav">
+                <li <?= $title == "Home" ? 'class="active"' : "" ?>>
+                    <a href="/home">
+                        <span class="glyphicon glyphicon-home" aria-hidden="true"></span>
+                        Home
+                        <span class="sr-only">(current)</span></a></li>
+                <li <?= $title == "Music" ? 'class="active"' : "" ?>>
+                    <a href="/music">
+                        <span class="glyphicon glyphicon-music" aria-hidden="true"></span>
+                        Music
+                    </a></li>
+                <li <?= $title == "Books" ? 'class="active"' : "" ?>>
+                    <a href="/books">
+                        <span class="glyphicon glyphicon-book" aria-hidden="true"></span>
+                        Books
+                    </a></li>
+                <li <?= $title == "Settings" ? 'class="active"' : "" ?>>
+                    <a href="/settings">
+                        <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+                        Settings
+                    </a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
 </body>
 
 <div class="container">
     <?php
-        $view['slots']->output('_content');
+    $view['slots']->output('_content');
     ?>
 </div>
